76a1182d59bc5a3b54fff7e147080c991f07ebde
Martin Alt <mnalt@gmx.de>
2016-06-21 19:13:19 +0200

    added packages, mirror and assert_grep

--- tests/test_create_jail_root.sh
+++ tests/test_create_jail_root.sh
@@ -5,6 +5,17 @@
 TEST_ROOT=/var/tmp/root_creation_tests
 BIN=../src
 
+
+function assert_grep () {
+	cmd=$1
+	shift
+	output=$(eval $cmd)
+
+	for pattern in "$@"; do
+		assert_raises "grep  -E -e \"$pattern\" <<< \"$output\"" 0
+	done
+}
+
 rm -rf "$TEST_ROOT"
 mkdir -p "$TEST_ROOT"
 
@@ -51,7 +62,17 @@ assert_end '"fail and display error message if target directory does not exist"'
 mkdir "$TEST_ROOT/new_root"
 CMD="$BIN/create_jail_root -n -v -a amd64 -r 10.3-RELEASE \"$TEST_ROOT/new_root\""
 assert_raises "$CMD" 0
-assert "$CMD" "Release: 10.3-RELEASE\nArchitecture: amd64\nTarget directory: $TEST_ROOT/new_root\nDry run only - not doing anything"
+assert "$CMD" "Release: 10.3-RELEASE\nArchitecture: amd64\nTarget directory: $TEST_ROOT/new_root\nMirror: ftp://ftp.freebsd.org\nPackages:\n - base\n - lib32\nDry run only - not doing anything"
 
 assert_end '"Parmeters are parsed correctly"'
+
+
+CMD="$BIN/create_jail_root -v -a amd64 -r 10.3-RELEASE \"$TEST_ROOT/new_root\""
+assert_grep "$CMD" ".*ackages:" " - base" " - lib32"
+
+assert_end '"Correct package sets are downloaded"'
+
+
+
+
 
